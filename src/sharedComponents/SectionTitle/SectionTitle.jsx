93009deeb2cd7c5bea6287da81f34a0795a879c0
JoujonikiAsa2 <joujonikiasaroy@gmail.com>
2023-11-25 22:43:23 +0600

    Added changes on section title design

--- src/sharedComponents/SectionTitle/SectionTitle.jsx
+++ src/sharedComponents/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const SectionTitle = ({heading,details}) => {
     return (
         <div className='text-center py-8'>
-            <h2 className='text-xl font-bold text-indigo-500 pb-3'>{heading}</h2>
+            <h2 className='text-3xl font-bold text-indigo-500'>{heading}</h2>
+            <div className='divider w-1/3 bg-purple-700 h-1 mx-auto'></div>
             <p className='text-xs'>{details}</p>
         </div>
     );
